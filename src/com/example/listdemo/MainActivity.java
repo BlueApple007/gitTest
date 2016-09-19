94af56c546fcf88d3c2c2713073c89902b7aa89c
BlueApple007 <1324501410@qq.com>
2016-09-19 16:07:55 +0800

    初次提交

---
+++ src/com/example/listdemo/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.listdemo;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends Activity {
+
+	private ListView lv;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		lv = (ListView) findViewById(R.id.list);
+		lv.setAdapter(new BaseAdapter() {
+			
+			@Override
+			public View getView(int position, View convertView, ViewGroup parent) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public long getItemId(int position) {
+				return 0;
+			}
+			
+			@Override
+			public Object getItem(int position) {
+				return null;
+			}
+			
+			@Override
+			public int getCount() {
+				return 0;
+			}
+			@Override
+			public int getViewTypeCount() {
+				return super.getViewTypeCount();
+			}
+			@Override
+			public int getItemViewType(int position) {
+				return super.getItemViewType(position);
+			}
+		});
+	}
+
+}
